a0635f65060a32dd511b8db1d12cd06aa5215bbf
gheja <gheja@m002560p.git.stdio.hu>
2019-08-30 01:52:56 +0200

    Player object now can be moved around

--- src/game_object_player.js
+++ src/game_object_player.js
@@ -9,4 +9,26 @@ class GameObjectPlayer extends GameObject
 		this.color = "#0e4";
 		this.gravity = true;
 	}
+	
+	tick()
+	{
+		if (_input.controls["up"].state && _input.controls["up"].changed)
+		{
+			this.speedY = -4;
+		}
+		
+		if (_input.controls["left"].state)
+		{
+			this.speedX += -4/10;
+		}
+		
+		if (_input.controls["right"].state)
+		{
+			this.speedX += 4/10;
+		}
+		
+		this.speedX *= 0.9;
+		
+		this.defaultTick();
+	}
 }
