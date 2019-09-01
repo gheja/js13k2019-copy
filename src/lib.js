068d583bf916a8658c9fa91f573e7d91d7fc1e56
gheja <gheja@m002560p.git.stdio.hu>
2019-09-01 18:13:06 +0200

    lib: removed a lot of unused functions, added a note about bindEvent()

--- src/lib.js
+++ src/lib.js
@@ -7,67 +7,9 @@ function _copy(obj)
 	return JSON.parse(JSON.stringify(obj));
 }
 
-function _merge(obj, settings)
-{
-	let i;
-	
-	if (typeof settings != "object")
-	{
-		return;
-	}
-	
-	for (i in obj)
-	{
-		if (!obj.hasOwnProperty(i))
-		{
-			continue;
-		}
-		
-		if (!settings.hasOwnProperty(i))
-		{
-			continue;
-		}
-		
-		obj[i] = settings[i];
-	}
-}
-
-function _z(x)
-{
-	return _gfx.zoomLevel * x;
-}
-
-function clamp(min, max, x)
-{
-	return Math.min(Math.max(x, min), max);
-}
-
-function pick(a)
-{
-	if (typeof a == "object")
-	{
-		return a[Math.floor(Math.random() * a.length)];
-	}
-	
-	return a;
-}
-
-function _getArrayKeys(a, keys)
-{
-	let i, result;
-	
-	result = {};
-	
-	for (i in keys)
-	{
-		result[keys[i]] = a[keys[i]];
-	}
-	
-	return result;
-}
-
 function bindEvent(obj, event, callback)
 {
+	// TODO: optimize for size: remove else {} as it is for pre-IE9 only
 	if (obj.addEventListener)
 	{
 		obj.addEventListener(event, callback);
@@ -76,9 +18,4 @@ function bindEvent(obj, event, callback)
 	{
 		obj.attachEvent("on" + event, callback);
 	}
-}
-
-function uniqueId()
-{
-	return (new Date()).getTime() + "" + Math.floor(Math.random() * 1000000);
 }
