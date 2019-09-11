d5dafa09de1f42f04e8f3ff1bc18562c44ef3d58
gheja <gheja@m002560p.git.stdio.hu>
2019-09-11 02:38:27 +0200

    GameObjectPlayer: added the graphics and basic animations

--- src/game_object_player.js
+++ src/game_object_player.js
@@ -74,6 +74,75 @@ class GameObjectPlayer extends GameObject
 		}
 	}
 	
+	draw()
+	{
+// 1: body front
+// 2: body front v2
+// 3: eye
+// 4: eye
+// 5: eye anim 1
+// 6: eye anim 2
+// 7: mouth
+// 8: leg front
+// 9: leg anim 1
+// 10: leg anim 2
+// 11: leg anim 3
+// 12: body left
+		if (Math.round(this.speedX, 2) == 0)
+		{
+		_gfx.drawShape(_shapes[2], this.x, this.y, 0.8, false);
+		
+		if (this.ticks % 300 < 190)
+		{
+			_gfx.drawShape(_shapes[7], this.x, this.y - 5, 0.3, false);
+		}
+		else
+		{
+			_gfx.drawShape(_shapes[7], this.x - 1, this.y - 4, 0.3, false);
+		}
+		
+		if (this.ticks % 140 < 130)
+		{
+			_gfx.drawShape(_shapes[4], this.x - 10, this.y - 10, 0.2, false);
+			_gfx.drawShape(_shapes[4], this.x + 10, this.y - 10, 0.2, false);
+		}
+		else if (this.ticks % 140 < 135)
+		{
+			_gfx.drawShape(_shapes[5], this.x - 10, this.y - 10, 0.2, false);
+			_gfx.drawShape(_shapes[5], this.x + 10, this.y - 10, 0.2, false);
+		}
+		else
+		{
+			_gfx.drawShape(_shapes[6], this.x - 10, this.y - 10, 0.2, false);
+			_gfx.drawShape(_shapes[6], this.x + 10, this.y - 10, 0.2, false);
+		}
+		
+		_gfx.drawShape(_shapes[8], this.x - 8, this.y + 16, 0.4, false);
+		_gfx.drawShape(_shapes[8], this.x + 8, this.y + 16, 0.4, false);
+		}
+		else
+		{
+			_gfx.drawShape(_shapes[12], this.x, this.y, 0.8, false);
+			_gfx.drawShape(_shapes[5], this.x - 10, this.y - 10, 0.2, false);
+			
+			if (this.ticks % 18 < 6)
+			{
+				_gfx.drawShape(_shapes[9], this.x - 2, this.y + 16, 0.2, false);
+				_gfx.drawShape(_shapes[10], this.x + 4, this.y + 16, 0.2, false);
+			}
+			else if (this.ticks % 18 < 12)
+			{
+				_gfx.drawShape(_shapes[10], this.x - 2, this.y + 16, 0.2, false);
+				_gfx.drawShape(_shapes[11], this.x + 4, this.y + 16, 0.2, false);
+			}
+			else
+			{
+				_gfx.drawShape(_shapes[11], this.x - 2, this.y + 16, 0.2, false);
+				_gfx.drawShape(_shapes[9], this.x + 4, this.y + 16, 0.2, false);
+			}
+		}
+	}
+	
 	tick()
 	{
 		let c;
