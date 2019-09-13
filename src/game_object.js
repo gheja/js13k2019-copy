cd8256347bfabd39f0d24957987eda5a6cd387e6
gheja <gheja@m002560p.git.stdio.hu>
2019-09-13 06:48:45 +0200

    Moved aabb() to lib

--- src/game_object.js
+++ src/game_object.js
@@ -59,17 +59,6 @@ class GameObject
 	{
 		let a;
 		
-		function aabb(o1, o2)
-		{
-			// Thanks super at StackOverflow: https://stackoverflow.com/a/25342644/460571
-			return !(
-				o1.x + o1.width  / 2 < o2.x - o2.width  / 2 ||
-				o1.y + o1.height / 2 < o2.y - o2.height / 2 ||
-				o1.x - o1.width  / 2 > o2.x + o2.width  / 2 ||
-				o1.y - o1.height / 2 > o2.y + o2.height / 2
-			);
-		}
-		
 		this.overlappedObjects.length = 0;
 		this.overlappedObjectBlocks = false;
 		
--- src/lib.js
+++ src/lib.js
@@ -19,3 +19,15 @@ function bindEvent(obj, event, callback)
 		obj.attachEvent("on" + event, callback);
 	}
 }
+
+// AABB collision/overlap detection
+function aabb(o1, o2)
+{
+	// Thanks super at StackOverflow: https://stackoverflow.com/a/25342644/460571
+	return !(
+		o1.x + o1.width  / 2 < o2.x - o2.width  / 2 ||
+		o1.y + o1.height / 2 < o2.y - o2.height / 2 ||
+		o1.x - o1.width  / 2 > o2.x + o2.width  / 2 ||
+		o1.y - o1.height / 2 > o2.y + o2.height / 2
+	);
+}
