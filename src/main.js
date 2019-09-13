a3c4b2f9b13c81b315e4b8bcfc3c0826c75680a0
gheja <gheja@m002560p.git.stdio.hu>
2019-09-13 03:36:39 +0200

    Added missing _transitionCtx

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 let _gfx;
 let _game;
 let _editor;
-let _zoom = 1.4;// should be multiples of 0.2
+let _zoom = 1.0;// should be multiples of 0.2
 let _padX = 0; // pixels
 let _padY = 0; // pixels
 let _canvasWidth = 1200;
 let _canvasHeight = 900;
 
-let _wallFrontCtx, _wallBackCtx;
+let _wallFrontCtx;
+let _wallBackCtx;
+let _transitionCtx;
 
 function init()
 {
